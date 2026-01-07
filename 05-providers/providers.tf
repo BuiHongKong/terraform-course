83c629ccaeae3ba47684b5894b7d9154a8605c7b
Kong <rd_kong@bpo-it.net>
2026-01-07 11:16:29 +0700

    commit 04

--- 05-providers/providers.tf
+++ 05-providers/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.7"
+  required_version = ">= 1.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,16 +12,9 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-provider "aws" {
-  region = "ap-southeast-1"
-  alias  = "ap-southeast-1"
-}
 
-resource "aws_s3_bucket" "eu_west_1" {
+
+resource "aws_s3_bucket" "my_bucket" {
   bucket = "some-random-bucket-name-aosdhfoadhfu"
 }
 
-resource "aws_s3_bucket" "us_east_1" {
-  bucket   = "some-random-bucket-name-18736481364"
-  provider = aws.ap-southeast-1
-}
