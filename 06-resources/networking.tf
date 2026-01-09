b3985969ecb47b2f68b1b9c68a8dd5f83ad42b3a
Kong <rd_kong@bpo-it.net>
2026-01-09 14:39:56 +0700

    commit 12

--- 06-resources/networking.tf
+++ 06-resources/networking.tf
@@ -2,16 +2,16 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "06-resources-vpc"
-    Project = "06-resources"
+    Name      = "06-resources-vpc"
+    Project   = "06-resources"
     ManagedBy = "Terraform"
   }
 
 }
 
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/24"
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
 
   tags = {
     Name      = "06-resources-public-subnet"
@@ -20,3 +20,33 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name      = "06-resources-internet-gateway"
+    Project   = "06-resources"
+    ManagedBy = "Terraform"
+  }
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+    tags = {
+    Name      = "06-resources-internet-gateway"
+    Project   = "06-resources"
+    ManagedBy = "Terraform"
+  }
+
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+
+}
